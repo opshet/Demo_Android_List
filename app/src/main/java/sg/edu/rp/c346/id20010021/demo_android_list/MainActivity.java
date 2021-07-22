1d7bdda4f160f818ff12671d713ff4ecd1d22f01
marcel <marcellim26@gmail.com>
2021-07-22 10:57:35 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20010021/demo_android_list/MainActivity.java
@@ -0,0 +1,49 @@
+package sg.edu.rp.c346.id20010021.demo_android_list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lv;
+    ArrayList<AndroidVersion> al;
+    //ArrayAdapter<AndroidVersion> aa;
+    CustomAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lv=findViewById(R.id.lv);
+        al= new ArrayList<>();
+
+        AndroidVersion item1 = new AndroidVersion("Pie", "9.0");
+        AndroidVersion item2 = new AndroidVersion("Oreo", "8.0 - 8.1");
+        AndroidVersion item3 = new AndroidVersion("Nougat", "7.0 - 7.1.2");
+
+        al.add(item1);
+        al.add(item2);
+        al.add(item3);
+
+
+//        aa = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, al);
+//        lv.setAdapter(aa);
+        adapter = new CustomAdapter(this, R.layout.row, al);
+        lv.setAdapter(adapter);
+
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                al.get(position);
+            }
+        });
+    }
+}
